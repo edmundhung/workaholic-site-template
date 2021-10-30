0a0cb2f219bbca313c7649b938352c1a663e7ef1
Edmund Hung <me@edmund.dev>
2021-10-30 18:22:43 +0200

    feat: make the demo site mobile friendly

--- site/src/Demo.tsx
+++ site/src/Demo.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import { useQuery } from 'react-query';
-import { BrowserRouter, Route, Routes, Outlet, useParams, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Outlet, useParams, NavLink } from 'react-router-dom';
 
 async function getContent(slug: string) {
-  const response = await fetch(`/data/${slug}`);
+  const response = await fetch(`/api/data/${slug}`);
   const data = await response.json();
 
   return data?.content ?? null;
 }
 
 async function listContents(path = '') {
-  const response = await fetch('/list');
+  const response = await fetch('/api/list');
   const data = await response.json();
 
   return data;
@@ -26,7 +26,7 @@ function Documentation() {
 
   return (
     <div className="p-4">
-      <ReactMarkdown className="prose">{data}</ReactMarkdown>
+      <ReactMarkdown className="prose prose-sm">{data}</ReactMarkdown>
     </div>
   );
 }
@@ -38,15 +38,15 @@ function Layout() {
   });
 
   return (
-    <div className="p-4 grid grid-cols-3 gap-4">
-      <main className="col-span-2">
+    <div className="p-4 sm:grid sm:grid-cols-4 gap-4">
+      <main className="sm:col-span-3 sm:col-start-2">
         <Outlet />
       </main>
-      <nav className="pt-12">
-        <ol>
+      <nav className="sm:pt-12 sm:col-start-1 sm:order-first">
+        <ol className="pt-6 sm:text-right sm:sticky sm:top-6">
           {data?.map(({ slug, metadata }: { slug: string, metadata: Record<string, any> }) => (
-            <li key={slug}>
-              <Link to={slug !== 'index' ? slug : '/'}>{metadata?.title}</Link>
+            <li key={slug} className="p-1">
+              <NavLink to={slug !== 'index' ? slug : '/'} className={({ isActive }) => isActive ? `font-bold` : ``}>{metadata?.title}</NavLink>
             </li>
           ))}
         </ol>
